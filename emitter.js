ee5d596d3b7c1a6cc0f1d1b939a77950d55c7513
malaev <30991434+malaev@users.noreply.github.com>
2018-11-08 14:05:03 +0500

    first

--- emitter.js
+++ emitter.js
@@ -4,40 +4,65 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let events = new Map();
+
     return {
 
-        /**
+        /*
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!events.has(event)) {
+                events.set(event, []);
+            }
+            events.get(event).push({
+                context: context,
+                handler: handler
+            });
+
+            return this;
         },
 
-        /**
+        /*
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (let key of events.keys()) {
+                if (key === event || key.startsWith(event + '.')) {
+                    events.set(event, events.get(event)
+                        .filter(person => person.context !== context));
+                }
+            }
+
+            return this;
         },
 
-        /**
+        /*
          * Уведомить о событии
          * @param {String} event
          */
         emit: function (event) {
-            console.info(event);
+            while (event !== '') {
+                if (events.has(event)) {
+                    events.get(event)
+                        .forEach(ev => ev.handler.call(ev.context));
+                }
+                event = event.substring(0, event.lastIndexOf('.'));
+            }
+
+            return this;
         },
 
         /**
